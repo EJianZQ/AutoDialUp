7129d7ef22a4b40d59a52be70fd3d6541ca8139c
辣条哥哥 <yyjpjs@gmail.com>
2022-10-18 19:49:21 +0800

    增加断开PPPoE连接的方法

--- DialUp/PPPoE.cs
+++ DialUp/PPPoE.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,6 +22,27 @@ namespace AutoDialUp
             Process p = new Process();//新建一个进程对象
             p.StartInfo.FileName = "Rasdial.exe";//设置要启动的进程名字
             p.StartInfo.Arguments = Name + " " + Account + " " + Password;//传递参数 格式 连接名字+空格+账号+空格+密码
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;//设置执行时的控制台为隐藏的
+            p.Start();//开始执行
+            p.WaitForExit();//等待连接后自动退出
+            if (p.ExitCode == 0)//通过退出返回的代码判断连接是否成功
+            {
+                return 1;
+            }
+            else
+                return 0;
+        }
+
+        /// <summary>
+        /// PPPoE拨号方式断开连接
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <returns></returns>
+        public static int DisConnect(string Name)
+        {
+            Process p = new Process();//新建一个进程对象
+            p.StartInfo.FileName = "Rasdial.exe";//设置要启动的进程名字
+            p.StartInfo.Arguments = Name + " /disconnect";
             p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;//设置执行时的控制台为隐藏的
             p.Start();//开始执行
             p.WaitForExit();//等待连接后自动退出
